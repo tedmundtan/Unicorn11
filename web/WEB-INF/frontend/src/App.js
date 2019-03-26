811ea2b96130685443f94afd65719df860fdb18e
nigel lee <lee.nigel@hotmail.com>
2019-03-26 11:01:40 +0800

    added frontend to web folder

--- web/WEB-INF/frontend/src/App.js
+++ web/WEB-INF/frontend/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <h1>hello</h1>
       </div>
     );
